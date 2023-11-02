ad4b4923038c250ae02295121b5d161b09c8880a
Logan Finnegan <logantfinnegan@protonmail.com>
2023-11-02 14:38:41 -0500

    refactor patron tests

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -12,9 +12,7 @@ Rspec.describe Patron do
 
   it "has readable attributes" do 
     expect(@patron_1.name).to eq("Bob")
+    expect(patron_1.spending_money).to eq(20)
+    expect(patron_1.interests).to eq([])
   end
-
-  it "can spend money" do 
-      expect(patron_1.spending_money).to eq(20)
-    end
 end   
