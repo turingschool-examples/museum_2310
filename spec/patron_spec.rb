96215fcd55a5b83b3c2ab656267a977fd8d51d25
Logan Finnegan <logantfinnegan@protonmail.com>
2023-11-02 14:44:04 -0500

    finish it 1 tests

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -15,4 +15,11 @@ Rspec.describe Patron do
     expect(patron_1.spending_money).to eq(20)
     expect(patron_1.interests).to eq([])
   end
+
+  it "can take new interests" do
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+
+    expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+  end
 end   
