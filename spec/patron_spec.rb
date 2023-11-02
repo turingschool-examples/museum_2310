3c89af9eb3a92e85202e9d359ae49a2dc10c4cfb
Nathan Lambertson <lambertones@gmail.com>
2023-11-02 14:17:49 -0500

    Test: Add tests for Patron class

---
+++ spec/patron_spec.rb
@@ -0,0 +1,30 @@
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe Patron do
+  it "exists" do
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1).to be_a(Patron)
+  end
+
+  it "has a name and spending money attribute" do
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1.name).to eq("Bob")
+    expect(patron_1.spending_money).to eq(20)
+  end
+
+  it "has an interests attribute" do
+    patron_1 = Patron.new("Bob", 20)
+
+    expect(patron_1.interests).to be_a(Array)
+  end
+
+  it "has a method that can add interests" do
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    expect(patron.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+  end
+end
