74165d6ea9f1948641d56159ec503e7a1652baf8
Martin Chavez <martinchavez1203@gmail.com>
2023-11-02 14:07:30 -0600

    add #recommend_exhibits method

--- lib/museum.rb
+++ lib/museum.rb
@@ -8,4 +8,11 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    recommend_exhibits = @exhibits.find_all do |exhibit|
+      patron.interests.include?(exhibit.name)
+    end
+    recommend_exhibits
+  end
 end
--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe Museum do
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
   end
 
   describe '#initialize' do
@@ -31,4 +33,20 @@ RSpec.describe Museum do
       expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls,@imax])
     end
   end
+
+  describe '#add_interest' do
+    it 'adds interests ' do
+      @dmns.add_exhibit(@dead_sea_scrolls)
+      @dmns.add_exhibit(@gems_and_minerals)
+      @dmns.add_exhibit(@imax)
+
+      @patron_1.add_interest("Dead Sea Scrolls")
+      @patron_1.add_interest("Gems and Minerals")
+      @patron_2.add_interest("IMAX")
+      expect(@dmns.recommend_exhibits(@patron_1)).to eq([@dead_sea_scrolls,@gems_and_minerals])
+      expect(@dmns.recommend_exhibits(@patron_2)).to eq([@imax])
+    end
+  end
+
+
 end
