433aa1b5e4b5f2f8f4d47d1381df083e75ed3c3d
Nathan Lambertson <lambertones@gmail.com>
2023-11-02 14:06:37 -0500

    Initial commit

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,22 @@
+require './lib/exhibit'
+require './lib/patron'
+
+RSpec.describe Exhibit do
+  it "exists" do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    expect(exhibit).to be_a(Exhibit)
+  end
+
+  it 'has a name attribute' do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    expect(exhibit.name).to eq("Gems and Minerals")
+  end
+
+  it "has a cost attribute" do
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    expect(exhibit.cost).to eq(0)
+  end
+end
