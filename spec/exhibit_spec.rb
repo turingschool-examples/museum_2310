b297d693ea836726fe46c5447ecbcba60ed931bc
Martin Chavez <martinchavez1203@gmail.com>
2023-11-02 13:01:06 -0600

    add exhibit class and test

---
+++ spec/exhibit_spec.rb
@@ -0,0 +1,21 @@
+require './lib/exhibit'
+# require './lib/patron'
+
+RSpec.describe Exhibit do
+  before(:each) do
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@exhibit).to be_a(Exhibit)
+    end
+  end
+
+  describe '#reads attributes' do
+    it 'returns and reads attributes' do
+      expect(@exhibit.name).to eq("Gems and Minerals")
+      expect(@exhibit.cost).to eq(0)
+    end
+  end
+end
