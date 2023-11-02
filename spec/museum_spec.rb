1d0a76aea37acf6ced218e2089e06480a5a07ffe
Martin Chavez <martinchavez1203@gmail.com>
2023-11-02 13:36:56 -0600

    add test for add_exhibit

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -5,6 +5,9 @@ require './lib/exhibit'
 RSpec.describe Museum do
   before(:each) do
     @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
   end
 
   describe '#initialize' do
@@ -17,6 +20,15 @@ RSpec.describe Museum do
     it 'reads and returns attributes' do
       expect(@dmns.name).to eq("Denver Museum of Nature and Science")
       expect(@dmns.exhibits).to eq([])
+    end
+  end
+
+  describe '#add_exhibit' do
+    it 'adds to the exhibits ' do
+      @dmns.add_exhibit(@gems_and_minerals)
+      @dmns.add_exhibit(@dead_sea_scrolls)
+      @dmns.add_exhibit(@imax)
+      expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls,@imax])
     end
   end
 end
